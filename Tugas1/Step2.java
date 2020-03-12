2551bf2daa1217b5aac10961b8d5dae7c53c8c50
Achmad Novanto <57305435+AchmadNovanto@users.noreply.github.com>
2020-03-12 16:43:39 +0800

    Add files via upload

---
+++ Tugas1/Step2.java
@@ -0,0 +1,5 @@
+public class Step2{
+	public static void main(String[] args){
+		System.out.print("Belajar Bahasa Pemrograman Java");
+	}
+}
---
+++ Tugas1/Step3.java
@@ -0,0 +1,5 @@
+public class Step3{
+	public static void main(String[] args){
+		System.out.print("Selesai Belajar Pemrograman Java");
+	}
+}
